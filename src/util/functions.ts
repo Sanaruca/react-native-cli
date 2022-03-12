750b44658c8190c74a94ed1541bf0f29fec47a4e
sanaruca <sanaruca@gmail.com>
2022-03-12 17:03:26 +0000

    test

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -2,7 +2,13 @@ import fs from "fs";
 import path from "path";
 
 export function createScreenFile(name: string) {
+  create(__dirname)
   fs.writeFile("./screen/test.ts", 'console.log("hola nene!!")', (error) => {
     if (error) throw error;
   });
+}
+
+function create(filePath: string) {
+  const condition = fs.existsSync(path.dirname(filePath))
+  console.log({condition})
 }
