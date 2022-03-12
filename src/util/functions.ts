102ec00b3ff5ccda14302dc3c04be5fe6d2d8281
sanaruca <sanaruca@gmail.com>
2022-03-12 17:40:41 +0000

    none

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -3,14 +3,14 @@ import path from "path";
 
 export function createScreenFile(name: string) {
   create(path.resolve('./dist/screen'))
-  fs.writeFile("./dist/screen/test.ts", 'console.log("hola nene!!")', (error) => {
+  fs.writeFile(`./dist/screen/${name}-screen.ts`, 'console.log("hola nene!!")', (error) => {
     if (error) throw error;
   });
 }
 
 function create(filePath: string) {
   const condition = fs.existsSync(filePath)
-  console.log({ condition, filePath, next: path.dirname(filePath) })
+  // console.log({ condition, filePath, next: path.dirname(filePath) })
   if (condition) return true;
   create(path.dirname(filePath))
   fs.mkdirSync(filePath)
