7751c15eea9abe2cddd29682760ae141b12ccac1
sanaruca <sanaruca@gmail.com>
2022-03-14 04:54:41 +0000

    none

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -2,23 +2,27 @@ import fs from "fs";
 import path from "path";
 
 export function createScreenFile(name: string) {
-  name = name.at(0)?.toUpperCase() + name.slice(1)
-  create(path.resolve('./dist/screens'))
+  name = name.at(0)?.toUpperCase() + name.slice(1);
+  create(path.resolve("./dist/screens"));
+  create(path.resolve("./dist/screens"));
   // console.log({__dirname})
-  const text = fs.readFileSync(path.join(__dirname, '/templates/screen-component-template.tsx'))
-  .toString()
-  .replace(/\$SCREEN_NAME\$/g, name)
+  const text = fs
+    .readFileSync(
+      path.join(__dirname, "/templates/screen-component-template.dott")
+    )
+    .toString()
+    .replace(/\$SCREEN_NAME\$/g, name);
   fs.writeFile(`./dist/screens/${name}-screen.tsx`, text, (error) => {
     if (error) throw error;
   });
 }
 
 function create(filePath: string) {
-  const condition = fs.existsSync(filePath)
+  const condition = fs.existsSync(filePath);
   // console.log({ condition, filePath, next: path.dirname(filePath) })
   if (condition) return true;
-  create(path.dirname(filePath))
-  fs.mkdirSync(filePath)
+  create(path.dirname(filePath));
+  fs.mkdirSync(filePath);
+  console.log("created:", filePath);
   // fs.mkdirSync(filePath)
-  console.log('---------')
 }
